acf868487c632f5dbdeeea8e62d43bf61d183814
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-07-09 10:22:55 +0100

    RSplit functions into small one purpose methods so it is easy to unit test

---
+++ pkg/metrics/responses_test.go
@@ -0,0 +1,14 @@
+package metrics
+
+import (
+	"testing"
+)
+
+func TestGetChannelID(t *testing.T) {
+}
+
+func TestGetMessagesHistory(t *testing.T) {
+}
+
+func TestGetFirstReplyTimestamp(t *testing.T) {
+}
